5a79aa0860772f06f7b3d5b70354b52ff3ab3823
Kai Prikulis <kai@radiantbounce.com>
2021-09-03 00:24:41 +1000

    Revert "Changing Matrix class, still broken"
    This reverts commit 76d17cf8e3a2441efaa76159ec92eb20c5ddd05d.

--- matrix_classes.py
+++ matrix_classes.py
@@ -1,45 +1,69 @@
+# Stores solutions to a coordinate
 class Point():
-
     def __init__(self, value=0):
         # A point is fixed when it is solved
-        if value == 0:   
-			# Not solved so initialise with all possible numbers
+        self.fixed = (value != 0)
+
+        if not self.fixed:   
+            # Not solved so initialise with all possible numbers
             self.values = {1,2,3,4,5,6,7,8,9}
         else:
             self.values = {}
-	 
-	def IsFixed(self):
-		return (len(self.values) == 1)
-		
+        self.value = value
 
     # ToDo: CHANGE POP TO REMOVE
-    def Remove(self, value):
-        if not IsFixed():
+    def Pop(self, value):
+        if not self.fixed:
             # Remove value for values set
-            self.values.discard(value)   
+            self.values.discard(value)
+            if len(self.values) == 1:
+                # There is only 1 value remaining
+                # This point is solved
+                self.value = self.values.pop()
+                self.fixed = True
 
+# Stores a 3x3 grid of Points
+class Grid():
+
+    def __init__(self):
+        self.map = [[{} for i in range(3)] for j in range(3)]
+        # Stores all numbers in the grid
+        self.values = []    # Maybe change to set
+
+    # Adds a point to self at coordinates (row, column)
+    def Add(self, point, row, column):
+        self.map[row][column] = point
+        if point.fixed:
+            # Point is solved
+            self.values.append(point.value)
+            return 1
+        return 0
+
+    # Returns if a grid contains a value
+    def Contains(self, value):
+        return value in self.values
+
+# Stores a 3x3 grid of Grid
 class Map():
 
-	def __init__(self):
-		self.map = [[Point() for i in range(9)] for j in range(9)]
-		self.solved = 0
-
-	def UpdatePoint(self, point, row, column):
-		self.map[row][column] = point
-	
-	def CheckSegment(self, row, column):
-		for i in range(row, row + 2):
-			for j in range(column, column + 2):
-
-
-	def CheckRow(self, row):
-		for i in range(row):
-
-
-	def CheckCol(self, col):
-		for j in range(col):
-
-
-	def CheckPoint(self, value, row, column):
-		return CheckCol(col) and CheckRow(row) and CheckSegment(3 * (row // 3), 3 * (col // 3))
-
+    def __init__(self):
+        self.map = [[Grid() for i in range(3)] for j in range(3)]
+        self.rows = [[] for i in range(9)]
+        self.columns = [[] for i in range(9)]
+        self.solved = 0
+    
+    # Add a point to self at (row, column)
+    def Add(self, point, row, column):
+        self.rows[row].append(point)
+        self.columns[column].append(point)
+        self.solved += self.map[row // 3][column // 3].Add(point, row % 3, column % 3)
+    
+    # Check if a Map contains value in row, column and grid
+    def CrossCheck(self, value, row, column):
+        for i in self.rows[row]:
+            if value == i.value:
+                return True
+        for i in self.columns[column]:
+            if value == i.value:
+                return True
+        return self.map[row // 3][column // 3].Contains(value)
