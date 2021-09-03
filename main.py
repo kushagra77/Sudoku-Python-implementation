3b3d479964e7a764885a4b6fa712eaeebb313e39
Kushagra77 <kushagrazaveri@gmail.com>
2021-09-03 16:28:03 +1000

    FULLY FINISHED, added guesscheck, outcheck features, finishes every puzzle within 13 iteration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sys import implementation
+import copy
 import matrix_classes as mc
 
 def main():
@@ -14,19 +15,73 @@ def main():
 		for column in range(9):
 			my_map.Add(mc.Point(int(linelist[row][column])), row, column)
 
+	mainloop(my_map)
 
+def mainloop(my_map, recursed=False):
 	
+	my_map.Print()
+	count = 0
 	while my_map.solved < 81:
+		if my_map.Lost():
+			return False
+
+		count += 1
+		if count % 5 == 0:
+			if not recursed:
+				my_map.Print()
+				guess(my_map)
+			else:
+				return True
 		for row in range(9):
 			for column in range(9):
 				point = my_map.rows[row][column]
+				
 				if not point.fixed:
 					for value in range(1, 10):
+						if value not in point.values:
+							continue
 						if my_map.CrossCheck(value, row, column):
-							my_map.solved += my_map.rows[row][column].Pop(value)
+							my_map.solved += my_map.rows[row][column].Pop(value)	
+						elif my_map.OutCheck(value, row, column):
+							my_map.solved += 1
+							my_map.RemoveInstance(value, row, column)
+							break
+	if recursed:
+		return True
 	my_map.Print()
+	print(count)
 	
-
+	
+def guess(old_map):
+	fullbreak = False
+	my_map = copy.deepcopy(old_map)
+	old_point, first, second = None, None, None
+	row, column = 0, 0
+	for i in range(9):
+		for j in range(9):
+			point = my_map.rows[i][j]
+			if len(point.values) == 2:
+				for k in point.values:
+					first = second
+					second = k
+				old_point = old_map.rows[i][j]
+				point.Pop(first)
+				old_point = old_map.rows[i][j]
+				row, column = i, j
+				fullbreak = True
+				break
+		if fullbreak:
+			break
+	if not mainloop(my_map, True):
+		
+		old_map.solved += old_point.Pop(second)
+		old_map.Print()
+		old_map.RemoveInstance(first, row, column)
+	else:
+		old_map.solved += old_point.Pop(first)
+		
+		old_map.RemoveInstance(second, row, column)
+	
 							
 if __name__ == "__main__":
     main()
--- matrix_classes.py
+++ matrix_classes.py
@@ -8,14 +8,14 @@ class Point():
             # Not solved so initialise with all possible numbers
             self.values = {1,2,3,4,5,6,7,8,9}
         else:
-            self.values = {}
+            self.values = set()
         self.value = value
 
     # ToDo: CHANGE POP TO REMOVE
     def Pop(self, value):
         # Remove value for values set
         self.values.discard(value)
-        if len(self.values) == 1:
+        if len(self.values) == 1 and not self.fixed:
             # There is only 1 value remaining
             # This point is solved
             self.value = self.values.pop()
@@ -27,23 +27,56 @@ class Point():
 class Grid():
 
     def __init__(self):
-        self.map = [[{} for i in range(3)] for j in range(3)]
-        # Stores all numbers in the grid
-        self.values = []    # Maybe change to set
+        self.map = [[set() for i in range(3)] for j in range(3)]
+          
+        
 
     # Adds a point to self at coordinates (row, column)
     def Add(self, point, row, column):
         self.map[row][column] = point
         if point.fixed:
             # Point is solved
-            self.values.append(point.value)
+            
             return 1
         return 0
+    
+    def Rectify(self, value):
+        
+        count = 0
+        for i in range(3):
+            for j in range(3):
+                count += self.map[i][j].Pop(value)
+
+        return count
+        
 
     # Returns if a grid contains a value
     def Contains(self, value):
-        return value in self.values
+        for row in self.map:
+            for point in row:
+                if point.value == value:
+                    return True
+        return False
 
+    def Lost(self):
+        values = []
+        for row in self.map:
+            for point in row:
+                if point.fixed:
+                    values.append(point.value)
+        if len(values) > len(set(values)):
+            return True
+        return False
+
+    def OutCheck(self, value, row, column):
+        
+        for i in range(3):
+            for j in range(3):
+                if (i != row or j != column) and value in self.map[i][j].values:
+                    return False        
+        return True
+        
+        
 # Stores a 3x3 grid of Grid
 class Map():
 
@@ -62,23 +95,76 @@ class Map():
     # Check if a Map contains value in row, column and grid
     def CrossCheck(self, value, row, column):
         for i in self.rows[row]:
+            
             if value == i.value:
                 return True
         for i in self.columns[column]:
+            
             if value == i.value:
                 return True
+
+        
         return self.map[row // 3][column // 3].Contains(value)
+
+            
+
     
     def Print(self):
-        print("-----------------------")
+        print("|-----------------------|")
         for i in range(9):
+            print("|", end = " ")
             for j in range(9):
                 print(self.rows[i][j].value, end=" ")
                 if j % 3 == 2:
                     print("|", end = " ")
             print("")
             if i % 3 == 2:
-                print("-----------------------")
+                print("|-----------------------|")
 
+    def RemoveInstance(self, value, row, column):
+        for point in self.rows[row]:
+            self.solved += point.Pop(value)
+                
+        for point in self.columns[column]:
+            self.solved += point.Pop(value)
+                
+        self.solved += self.map[row // 3][column // 3].Rectify(value)
+
+    def RowCheck(self, value, row):
+        for i in range(9):
+            if i != row and value in self.rows[row][i].values:
+                return False
+        return True
+
+    def ColumnCheck(self, value, column):
+        for i in range(9):
+            if i != column and value in self.columns[column][i].values:
+                return False
+        return True
         
-        
+    def OutCheck(self, value, row, column):
+        point = self.map[row//3][column//3].map[row%3][column%3]
+        condition = self.RowCheck(value, row) or self.ColumnCheck(value, column)
+        condition = condition or self.map[row//3][column//3].OutCheck(value, row%3, column%3)
+
+        if condition:
+            point.value = value
+            point.values &= set()
+            point.fixed = True
+            return True
+        return False
+
+    def Lost(self):
+        for i in range(9):
+            row = [j.value for j in self.rows[i] if j.fixed]
+            
+            if len(row) > len(set(row)):
+                return True
+            column = [j.value for j in self.columns[i] if j.fixed]
+            
+            if len(column) > len(set(column)):
+                return True
+            if self.map[i//3][i%3].Lost():
+                return True
+        return False
+                
